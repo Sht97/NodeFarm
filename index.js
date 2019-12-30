50e41d9008801d3fbc74fc006dc4f3068d80f799
Daniel Rivera <Danielf.r97@gmail.com>
2019-12-30 10:50:36 -0500

    Sync and A-sync

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+
+// //BLOCKING: Función syncrona, espera a que se ejecute una línea de código para pasar a la siguiente
+// const textInput=fs.readFileSync('./txt/input.txt','utf-8');
+// //console.log(textInput);
+//
+// const textToWrite= `Testing the template strings with var include ${textInput}.
+// Create on ${Date.now()}`;
+// fs.writeFileSync('./txt/Output1.txt',textToWrite);
+// //console.log("Save win");
+//
+//  //Non blocking, A-syncrona
+// fs.readFile('./txt/start.txt','utf-8',(error,data)=>{
+//     fs.readFile(`./txt/${data}.txt`,'utf-8',(error,data1)=> {
+//         fs.readFile('./txt/append.txt','utf-8',(error,data2)=> {
+//             console.log(data1,data2);
+//
+//             fs.writeFile('./txt/finally.txt',data1+data2 ,'utf-8',err => {
+//                 if (err) console.log("Problem saving");
+//                 else console.log("Saved successful")
+//             })
+//         });
+//     });
+// });
+// console.log("hi from out");
