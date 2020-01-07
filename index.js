5f4ebb6c917d1a30ca62246b67a98eaeb011a80d
Daniel Rivera <Danielf.r97@gmail.com>
2020-01-07 14:07:03 -0500

    Nodemon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const http =require('http');
-
+const http = require('http');
+const url = require('url');
 ////////////////////////FILES///////////////////
 // //BLOCKING: Función syncrona, espera a que se ejecute una línea de código para pasar a la siguiente
 // const textInput=fs.readFileSync('./txt/input.txt','utf-8');
@@ -26,9 +26,31 @@ const http =require('http');
 // });
 // console.log("hi from out");
 ////////////////////////SERVER///////////////////
-const server = http.createServer(((req, res) => {
-    res.end('Hello from server')
-}));
+const jsonFile=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+
+const server = http.createServer((req, res) => {
+    const path=req.url;
+    if(path === '/overview' ||path === '/'){
+        res.end('this is a OVERVIEW')
+    }
+    else if(path === '/product'){
+        res.end('Hello from product page')
+    }
+    else if(path === '/API'){
+        res.writeHead(200,{
+            'Content-type':'application/json'
+        });
+        res.end(jsonFile);
+    }
+
+    else {
+        res.writeHead(404,{
+            'Content-type':'text/html',
+            'My-own-header':'Hello-World'
+        });
+        res.end('<h1>404 page not found</h1>');
+    }
+});
 server.listen(3000,()=>{
     console.log("listen on port 3000");
 });
